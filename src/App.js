30643235a76d803c875b6b95897419302757e9f6
Theo Asvestopoulos <t.v.asvestopoulos@gmail.com>
2022-07-21 22:03:51 +0200

    Design the interface

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import "./button.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div id="quote-box">
+        <q id="text" className="text">
+          This is a quote.
+        </q>
+        <p id="author" className="text">
+          Mark Example
+        </p>
+        <button className="button" id="new-quote">
+          New Quote
+        </button>
+        <a className="button" id="tweet-quote" href="_blank">
+          Tweet Quote
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default App;
